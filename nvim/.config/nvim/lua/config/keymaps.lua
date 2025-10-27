12b03875ca43896d6fe15181083f964c9a0d42ae
Diego <diego.salamanca0205@gmail.com>
2025-10-26 21:45:16 -0500

    Improve workflow

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,13 +3,39 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<leader>wh', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<leader>wl', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<leader>wj', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<leader>wk', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<S-h>', '<cmd>tabprevious<CR>')
+vim.keymap.set('n', '<S-l>', '<cmd>tabnext<CR>')
+vim.keymap.set('n', '<C-w>', '<cmd>tabclose<CR>')
+vim.keymap.set('n', '<C-t>', '<cmd>tabnew<CR>')
+
+vim.keymap.set('n', '<leader>q', '<cmd>quit<CR>')
+vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>')
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+vim.keymap.set('n', '<leader>xx', '<cmd>lua vim.diagnostic.setqflist()<CR>')
+
+vim.keymap.set('n', '<leader>es', '<cmd>set spell spelllang=es<CR>')
+
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
 vim.keymap.set('n', '[d', function()
   vim.diagnostic.jump { count = -1, float = true }
 end)
+
 vim.keymap.set('n', ']d', function()
   vim.diagnostic.jump { count = 1, float = true }
 end)
+
 vim.keymap.set('n', '[h', function()
   gitsigns.nav_hunk 'prev'
 end)
@@ -17,6 +43,7 @@ end)
 vim.keymap.set('n', ']h', function()
   gitsigns.nav_hunk 'next'
 end)
+
 vim.keymap.set('n', '<leader>fw', '<CMD>noautocmd w<CR>')
 vim.keymap.set({ 'n', 'v', 'x' }, 'j', 'gj')
 vim.keymap.set({ 'n', 'v', 'x' }, 'k', 'gk')
